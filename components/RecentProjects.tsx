edac63911f7fbaffa04f1e178320adcb11211dae
Ujjawal <121127299+Ujjwal807@users.noreply.github.com>
2025-05-15 23:16:52 +0530

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -8,7 +8,7 @@ import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
